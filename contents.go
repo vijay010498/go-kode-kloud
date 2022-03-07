6e54822aa07ed351652c8496cb95f4e9f8df7673
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2022-03-07 12:21:49 +0530

    variable declaration

---
+++ contents.go
@@ -0,0 +1,19 @@
+package main
+
+// Every go program starts with package declaration.
+// Package can be shared as a library
+// import to include code from other packages.
+// "fmt" -Input and output
+// Main is a special package
+// main func is the stating point inside main package
+// Every pgm should contain a main and main func
+
+// Data Types
+// staticT Typed - Compiler throws an error wen types are used incorrectly. Ex. C++, Java
+// Adv :  Better performance, Data integrity
+// Dynamic Typed - JS, Python - Compiler does not enforce the type system
+// ADV. Faster to write code, Shorter learning curve
+
+// Golang -  Either explicitly declaration ot inferred by compiler,
+// - Fast, statically typed compiled language feels like dynamically typed
+// Shot variable declaration // name:= "Vijay"
